734e75a1ec9fbb80f6919d6eade74773fa4472be
Tom Coyne <tcoyne1729@gmail.com>
2026-03-05 10:39:26 +0700

    unbatch

--- common_fns.py
+++ common_fns.py
@@ -1,13 +1,10 @@
 
 def formatting_prompts_func(example: dict):
     print(list(example.keys()))
-    output_texts = []
-    for i in range(len(example['question'])):
-        text = (
-            "Generate SQL for this question."
-            f"### schema\n{example['schema'][i]}\n"
-            f"### question\n{example['question'][i]}\n"
-            f"### sql\n{example['query'][i]}"
-        )
-        output_texts.append(text)
-    return output_texts
+    text = (
+        "Generate SQL for this question."
+        f"### schema\n{example['schema']}\n"
+        f"### question\n{example['question']}\n"
+        f"### sql\n{example['query']}"
+    )
+    return text
